9ef84e6bcde94a3da39d6eb5a8f886568fe7b682
abdessadek <abdessadekbirama1@gmail.com>
2024-11-02 09:22:08 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="myProjects">
         <Routes>
             {/* <Route path="/" element={ <Home />}> */}
                 <Route index element={<ChatPage/>} />
